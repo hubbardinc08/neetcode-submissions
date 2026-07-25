f6edf19cfcc815c837f1f67b77e6931c8ee0626d
Howard Qian <123795035+hubbardinc08@users.noreply.github.com>
2026-07-24 19:31:42 -0700

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.replace(" ", "")
+        s = s.lower()
+        s = re.sub(r'[^a-zA-Z0-9]', '', s)
+        left_pointer = 0
+        right_pointer = len(s) - 1
+        print(f"DEBUG: left pointer: {left_pointer}, right pointer: {right_pointer}")
+
+        while (left_pointer < right_pointer):
+            print(f"DEBUG:  left: {s[left_pointer]}, right: {s[right_pointer]}")
+            if (s[left_pointer] != s[right_pointer]):
+                return False
+            
+            left_pointer += 1
+            right_pointer -= 1
+        
+        return True
